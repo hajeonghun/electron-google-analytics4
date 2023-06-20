cd08ad16b68a395ccb4f22a4034895e471ea6f84
hajh1994@gmail.com <hajh1994@gmail.com>
2023-06-20 16:01:27 +0900

    test: 전송 테스트

--- __test__/analytics.test.ts
+++ __test__/analytics.test.ts
@@ -1,18 +1,14 @@
-import {describe, expect, test} from '@jest/globals';
+import {describe, expect, test, beforeEach, afterEach, beforeAll ,afterAll} from '@jest/globals';
 import Analytics from "../src/index";
 
-describe('module', () => {
-    test('analytics', async () => {
-        const analytics = new Analytics('G-MFB9FKCXVY','Rx4CgrGJRP-t936bqifyNw','1080');
-        analytics.set('serviceCode','jung')
-        analytics.set('app_name','kakaka')
-        analytics.setUserProperties({
-            COLLECT_CNO: {value: '111'},
-            COLLECT_COMPANY_NAME: {value: 'zzz'},
-            COLLECT_SERVICE: {value: 'erwrwer'},
-            COLLECT_USER_ID: {value: 'chmp'},
-            COLLECT_SITE_URL: {value: '#/main'},
-        })
-        console.log('result: ',analytics.event('page_view'))
+const trackingID = 'G-RANDOM';
+const secretKey = '000';
+
+describe('Analytics', () => {
+    test('should send a pageview request', async () => {
+        const analytics = new Analytics(trackingID,secretKey);
+        const response = await analytics.event('page_view');
+
+        expect(response.status).toBe(204)
     });
 });
